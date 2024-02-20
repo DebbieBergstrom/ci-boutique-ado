2b3fd5f8a86dd8e0327e6245362caed2cc4ef911
Debbie Bergstrom <debbiebergstroem@gmail.com>
2024-02-20 20:14:33 +0000

    Adjust settings for static files and environment handling

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -33,11 +33,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
+DEBUG = os.environ.get("DEVELOPMENT", "") == "True"
 
 ALLOWED_HOSTS = [
     "boutique-ado-ci-debbie.herokuapp.com",
     "localhost",
+    "127.0.0.1",
     "8000-debbiebergs-ciboutiquea-fx319ljskt2.ws-eu108.gitpod.io",
 ]
 
